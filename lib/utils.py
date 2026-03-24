8e08eb673d8a06b4254fd3bf9694f8c177f4fbae
Koos85 <koosjoosten@gmail.com>
2026-03-24 13:35:57 +0100

    fix typing

--- lib/utils.py
+++ lib/utils.py
@@ -25,7 +25,7 @@ def parse_custom_fields(custom_fields: list[vim.CustomFieldsManager.Value],
     return custom_field_data
 
 
-def _get_field_value(fields: dict[int, vim.CustomFieldsManager.Value],
+def _get_field_value(fields: dict[int, str | None],
                      key: int) -> str:
     try:
         value = fields[key]
