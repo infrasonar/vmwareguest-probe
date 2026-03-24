091d99bf04da17db50fa06c49de4bb3e8726b428
Koos85 <koosjoosten@gmail.com>
2026-03-24 13:21:12 +0100

    as str

--- lib/utils.py
+++ lib/utils.py
@@ -12,22 +12,26 @@ def datetime_to_timestamp(inp):
 def parse_custom_fields(custom_fields: list[vim.CustomFieldsManager.Value],
                         custom_field_keys: dict[str, int]
                         ) -> list[dict[str, Any]]:
+    custom_fields_lk = {
+        field.key: field.value  # type: ignore
+        for field in custom_fields
+    }
     custom_field_data = [{
         'name': name,
-        'value': _get_field_value(custom_fields, key)
+        'value': _get_field_value(custom_fields_lk, key)
     }
         for name, key in custom_field_keys.items()
     ]
     return custom_field_data
 
 
-def _get_field_value(custom_fields: list[vim.CustomFieldsManager.Value],
+def _get_field_value(fields: dict[int, vim.CustomFieldsManager.Value],
                      key: int) -> str:
     try:
-        key_field = [field for field in custom_fields if field.key == key][0]
+        key_field = fields[key]
         value = key_field.value  # type: ignore
-    except IndexError:
-        return  # TODO
+    except KeyError:
+        return f'<NO_INDEX_{key}>'
     if value is None:
-        return  # TODO
+        return '<NONE>'
     return str(value)
