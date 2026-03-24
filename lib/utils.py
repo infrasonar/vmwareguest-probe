ff09781ce617812ff52bc282cf44c84ab5bd98a4
Koos85 <koosjoosten@gmail.com>
2026-03-24 12:44:41 +0100

    typing

--- lib/utils.py
+++ lib/utils.py
@@ -9,7 +9,7 @@ def datetime_to_timestamp(inp):
     return calendar.timegm(inp.timetuple())
 
 
-def parse_custom_fields(custom_fields: vim.VirtualMachine.customValue,
+def parse_custom_fields(custom_fields: list[vim.CustomFieldsManager.Value],
                         custom_field_keys: dict[str, int]
                         ) -> list[dict[str, Any]]:
     custom_field_data = [{
@@ -21,13 +21,13 @@ def parse_custom_fields(custom_fields: vim.VirtualMachine.customValue,
     return custom_field_data
 
 
-def _get_field_value(custom_fields: vim.VirtualMachine.customValue,
+def _get_field_value(custom_fields: list[vim.CustomFieldsManager.Value],
                      key: int) -> str:
     try:
         key_field = [field for field in custom_fields if field.key == key][0]
-        value = key_field.value
+        value = key_field.value  # type: ignore
     except IndexError:
-        return
+        return  # TODO
     if value is None:
-        return
+        return  # TODO
     return str(value)
